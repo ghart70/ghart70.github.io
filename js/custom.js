10d7a72a16188c1c53a45c5ee01cd73f6d4e6ecb
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2024-05-21 15:06:52 -0500

    I hope this works like I think it does :)

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,29 @@ getYear();
 $(document).ready(function () {
     $('select').niceSelect();
 });
+
+/*
+  background-image: -webkit-gradient(linear, left top, right top, from(rgba(8, 19, 92, 0.9)), to(rgba(8, 19, 92, 0.9))), url(../images/michael.jpg);
+  background-image: -webkit-linear-gradient(left, rgba(8, 19, 92, 0.9), rgba(8, 19, 92, 0.9)), url(../images/michael.jpg);
+  background-image: -moz-linear-gradient(left, rgba(8, 19, 92, 0.9), rgba(8, 19, 92, 0.9)), url(../images/michael.jpg);
+  background-image: -o-linear-gradient(left, rgba(8, 19, 92, 0.9), rgba(8, 19, 92, 0.9)), url(../images/michael.jpg);
+  background-image: linear-gradient(to right, rgba(8, 19, 92, 0.9), rgba(8, 19, 92, 0.9)), url(../images/michael.jpg);
+*/
+
+function resolveAfter30Seconds() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 30000);
+    });
+}
+  
+async function asyncCall() {
+    alert('calling');
+    const result = await resolveAfter30Seconds();
+    alert(result);
+    asyncCall();
+    // Expected output: "resolved"
+}
+  
+asyncCall();
